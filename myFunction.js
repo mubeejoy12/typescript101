92a88cd7e18152f6d41f04591b3505456435f56e
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2025-03-17 11:58:46 +0100

    javascript of my typescript code

---
+++ myFunction.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    //   return num + 5;
+    return "hello";
+}
+function getUpper(val) {
+    return val.toLocaleUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+signUpUser("mubarak", "mubarak@gmail.com", false);
+getUpper("mubarak");
+var Myvalue = addTwo(10);
+loginUser("m", "m@m.com");
