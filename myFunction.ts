648f1a173f076ff5893c30d13936016a058a4be8
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2025-03-17 11:58:19 +0100

    learning how functions works within typescript when to add type at the argument and when to add type when returning value

---
+++ myFunction.ts
@@ -0,0 +1,23 @@
+function addTwo(num: number): number {
+  return num + 5;
+  //   return "hello";
+}
+
+function getUpper(val: string) {
+  return val.toLocaleUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
+
+
+signUpUser("mubarak", "mubarak@gmail.com", false);
+getUpper("mubarak");
+let Myvalue = addTwo(10);
+loginUser("m", "m@m.com");
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+export {};
