209da30a45c899b3d3ab6973ae7bc658e62cd98a
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2025-03-17 11:59:14 +0100

    learning variable in typescript

---
+++ variableMe.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Mubarak";
+greetings.toLocaleLowerCase();
+console.log(greetings);
+// number
+var userId = 334455.5;
+userId.toFixed();
+// boollean
+var isloggedIn = false;
--- variableMe.ts
+++ variableMe.ts
@@ -1,3 +1,21 @@
 let greetings: string = "Hello Mubarak";
+greetings.toLocaleLowerCase();
 
 console.log(greetings);
+
+// number
+let userId = 334455.5;
+userId.toFixed();
+
+// boollean
+let isloggedIn: boolean = false;
+
+// any
+let hero: string;
+
+function getHero() {
+  return "thor";
+}
+hero = getHero();
+
+export {};
