ce72c5e9ef87fc77767b3e229a24969e8ed5d9b0
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2025-03-18 16:05:09 +0100

    javacript of my object typescript

---
+++ myObject.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: "mubarak",
+    email: "mubarak@gmail.com",
+    isActive: true,
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+createUser({ name: "mubarak", isPaid: false });
